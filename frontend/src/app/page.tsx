7f2bae89f87a5462ea5283cfc1fa1597777fe23b
marcospedroweb <king.frontweb@outlook.com>
2026-07-07 20:53:25 -0300

    refactor: header css / list movies css

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
       <Header />
-      <main className="flex flex-1 w-full max-w-7xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+      <main className="flex flex-1 w-full max-w-7xl flex-col items-start justify-start py-24 px-16">
         <ListMovies />
       </main>
     </div>
