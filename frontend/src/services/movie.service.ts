445385ddeeea7b8fed04ed0e56e4aeb728f3e75a
marcospedroweb <king.frontweb@outlook.com>
2026-07-07 19:16:53 -0300

    feat: getById, update, rate and delete

--- frontend/src/services/movie.service.ts
+++ frontend/src/services/movie.service.ts
@@ -1,7 +1,7 @@
 import { api } from "./api";
 
 import { Movie } from "@/types/movie";
-import { CreateMovieDto } from "@/types/dto";
+import { CreateMovieDto, UpdateMovieDto } from "@/types/dto";
 
 export const movieService = {
   async create(data: CreateMovieDto): Promise<Movie> {
@@ -13,5 +13,24 @@ export const movieService = {
   async getAll(): Promise<Movie[]> {
     const response = await api.get<Movie[]>("/movies");
     return response.data;
+  },
+
+  async getById(id: number): Promise<Movie> {
+    const response = await api.get<Movie>(`/movies/${id}`);
+    return response.data;
+  },
+
+  async update(id: number, data: UpdateMovieDto): Promise<Movie> {
+    const response = await api.put<Movie>(`/movies/${id}`, data);
+    return response.data;
+  },
+
+  async rate(id: number, rating: number, watched: boolean): Promise<Movie> {
+    const response = await api.post<Movie>(`/movies/${id}/watch`, { rating, watched });
+    return response.data;
+  },
+
+  async delete(id: number): Promise<void> {
+    await api.delete(`/movies/${id}`);
   }
 }
